fe4da8d975c8fc94b47b2a6d1374a2d85ec941c9
kai <kai.wang.john@gmail.com>
2020-08-26 16:31:10 +0800

    修改图标和夜间模式

--- _src/index.js
+++ _src/index.js
@@ -5,8 +5,8 @@ import Tobi from 'rqrauhvmra__tobi'
 const tobi = new Tobi()
 
 // Remove the following lines to remove the darkmode js
-import Darkmode from 'darkmode-js'
-function addDarkmodeWidget() {
-  new Darkmode().showWidget()
-}
-window.addEventListener('load', addDarkmodeWidget)
+// import Darkmode from 'darkmode-js'
+// function addDarkmodeWidget() {
+//   new Darkmode().showWidget()
+// }
+// window.addEventListener('load', addDarkmodeWidget)
